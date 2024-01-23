d20ba3f1ed53f65ead3ce909ffe6809113c751d2
NhatLong213 <140641044+NhatLong213@users.noreply.github.com>
2024-01-23 13:57:10 +0700

    ll

---
+++ HelloWorld/src/ntu/edu/XinChao.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package ntu.edu;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Xin chao, toi la sinh vien khoa 63 CNTT - NTU: Hoc lap trinh Java");
+		System.out.print("NguyenNhatLong");
+
+	}
+
+}
